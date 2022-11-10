0cc9474c6efcb9170648fc07978405d6eac6bf43
Gennady Badinov <gennady.badinov@gmail.com>
2022-11-10 11:13:18 +0300

    Add create `item` endpoint

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from enum import Enum
+from pydantic import BaseModel
+
 from fastapi import FastAPI
 
 api = FastAPI()
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @api.get("/")
@@ -33,17 +32,11 @@ async def read_user(user_id: int):
     return {"user": user_id}
 
 
-@api.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "resnet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
 @api.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@api.post("/items/")
+async def create_item(item: Item):
+    return item
