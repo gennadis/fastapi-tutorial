92ca4859cd978d931eab78dc624188705e7cb969
Gennady Badinov <gennady.badinov@gmail.com>
2022-11-10 10:04:13 +0300

    Add `models`endpoint with enum choices

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from enum import Enum
 from fastapi import FastAPI
 
 api = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @api.get("/")
@@ -21,3 +28,14 @@ async def read_user_me():
 @api.get("/users/{user_ud}")
 async def read_user(user_id: int):
     return {"user": user_id}
+
+
+@api.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "resnet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
