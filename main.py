0c33b8a1c60fdb45b3ed7784acae26437381aa0e
Gennady Badinov <gennady.badinov@gmail.com>
2022-11-10 09:56:38 +0300

    Add read `users` endpoints

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ async def root():
 @api.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@api.get("/users/me")
+async def read_user_me():
+    return {"user": "the current user"}
+
+
+@api.get("/users/{user_ud}")
+async def read_user(user_id: int):
+    return {"user": user_id}
