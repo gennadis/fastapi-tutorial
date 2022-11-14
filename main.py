0056736489f6480dc8caa449a996233392318b9b
Gennady Badinov <gennady.badinov@gmail.com>
2022-11-14 08:12:06 +0300

    Add int parameters validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 
 api = FastAPI()
 
@@ -17,9 +17,9 @@ async def root():
     return {"message": "Hello, world"}
 
 
-@api.get("/items/{item_id}")
-async def read_item(item_id: int, needy: str, skip: int, limit: int | None = None):
-    return {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+# @api.get("/items/{item_id}")
+# async def read_item(item_id: int, needy: str, skip: int, limit: int | None = None):
+#     return {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
 
 
 @api.get("/users/me")
@@ -37,17 +37,9 @@ async def read_file(file_path: str):
     return {"file_path": file_path}
 
 
-@api.get("/items/")
-async def read_items(
-    q: list[int]
-    | None = Query(
-        default=[1, 3, 1999],
-        title="List of integers",
-        description="List of integers",
-        deprecated=True,
-    )
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+@api.get("/items/{item_id}")
+async def read_item(q: str, item_id: int = Path(title="ITEM ID", ge=1, le=10)):
+    results = {"items": ["foo", "bar"], "things": ["bar", "foo"], "item_id": item_id}
     if q:
         results.update({"q": q})
     return results
